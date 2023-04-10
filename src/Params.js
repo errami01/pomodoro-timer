70960a370b38628cc032d3066bb2d78489fba691
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-04-10 07:25:29 +0000

    Add params label

---
+++ src/Params.js
@@ -0,0 +1,9 @@
+
+function Params({id, label}){
+    return(
+        <div className="params-container">
+            <h2 id={id} className="params-label">{label}</h2>
+        </div>
+    )
+}
+export default Params
