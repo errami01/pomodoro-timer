43fa432678d65b5478c79dc2da26fcbb45dfb349
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-04-14 15:08:48 +0000

    fix all tests bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import { useState, useEffect, useRef} from 'react';
-import Params from './Params';
-const DEFAULT_SESSION = 1;
-const DEFAULT_BREAK = 1
+import './App.css'
+import { useState, useEffect, useRef} from 'react'
+import Params from './Params'
+
 function App() {
 // const [sessionDuration, setSessionDuration] = useState(1)
 // const [breakDuration , setBreakDuration] = useState(5)
+const DEFAULT_SESSION = 25
+const DEFAULT_BREAK = 5
 const [timer, setTimer] = useState({
   turn: 'session', 
   sessionLength: DEFAULT_SESSION, 
@@ -19,6 +20,7 @@ const secElement = useRef()
 const intervalId = useRef(null)
 const remainingTime = useRef(timer.sessionLength*60)
 const beep = useRef()
+const timerLabel = timer.turn==="session"? "Session":"Break"
 const displayedTimer = timer.turn === 'session'? timer.sessionLength<10? `0${timer.sessionLength}`: timer.sessionLength
                                                 : timer.breakLength<10? `0${timer.breakLength}`:timer.breakLength
 // console.log(minutesElement.current.innerHTML)
@@ -36,8 +38,11 @@ function handlePlayPauseClick(){
  
 }
 function handleResetClick(){
+  beep.current.pause()
+  beep.current.currentTime = 0
   setTimer(prev=>({
     ...prev,
+    turn: 'session',
     sessionLength: DEFAULT_SESSION,
     breakLength: DEFAULT_BREAK,
     play: false,
@@ -47,59 +52,81 @@ function handleResetClick(){
 }
 useEffect(()=>{
   // beep.current.play()
+  clearInterval(intervalId.current)
+  if(timer.sessionLength <0 ){
+    setTimer(prev=>({
+      ...prev,
+      sessionLength: 1
+    }))
+  }
   if(timer.turn === 'session') {
     remainingTime.current = timer.sessionLength*60
-    minutesElement.current.innerHTML = timer.sessionLength <10? `0${timer.sessionLength}`: timer.sessionLength
+    minutesElement.current.innerHTML = timer.sessionLength <10? `0${timer.sessionLength}:00`: timer.sessionLength+':00'
   }
   else {
     remainingTime.current = timer.breakLength*60
-    minutesElement.current.innerHTML = timer.breakLength <10? `0${timer.breakLength}`:timer.breakLength
+    minutesElement.current.innerHTML = timer.breakLength <10? `0${timer.breakLength}:00`: timer.breakLength+':00'
   }
-  secElement.current.innerHTML = "00"
-  clearInterval(intervalId.current)
+  // secElement.current.innerHTML = "00"
+  
 
   intervalId.current = setInterval(()=>{
     if(timer.play){
-      if( remainingTime.current <= 0) {
+      if( remainingTime.current == 0) {
         beep.current.play()
-        clearInterval(intervalId.current)
         setTimer(prev=>({
           ...prev,
           turn: prev.turn==='session'? 'break':'session'
         }))
+        clearInterval(intervalId.current)
         return
     }
   remainingTime.current = remainingTime.current -1
   const minutes = Math.floor(remainingTime.current/60);
   const sec = remainingTime.current%60
-  minutesElement.current.innerHTML = minutes <10? `0${minutes}`: minutes
-  secElement.current.innerHTML = sec < 10 ? `0${sec}`:sec}
+  const minuteToDisplay = (minutes <10? `0${minutes}`: minutes)
+  const secToDisplay= (sec < 10 ? `0${sec}`:sec)
+  minutesElement.current.innerHTML = minuteToDisplay+`:`+secToDisplay
+  // minutesElement.current.innerHTML = minutes <10? `0${minutes}`: minutes 
+  // secElement.current.innerHTML = sec < 10 ? `0${sec}`:sec
+}
     
-  },1000)
+  },100)
 },[timer.turn==='session'? timer.sessionLength:timer.breakLength,timer.turn,timer.reset])
 useEffect(()=>{
   
     
     clearInterval(intervalId.current)
+    if(timer.sessionLength <0 ){
+      setTimer(prev=>({
+        ...prev,
+        sessionLength: 1
+      }))
+    }
     intervalId.current = setInterval(()=>{
       if(timer.play){
         // clearInterval(intervalId.current)
-        if( remainingTime.current <= 0) {
+        if( remainingTime.current == 0) {
           beep.current.play()
-          clearInterval(intervalId.current)
           setTimer(prev=>({
             ...prev,
             turn: prev.turn==='session'? 'break':'session'
           }))
+          clearInterval(intervalId.current)
           return
       }
     remainingTime.current = remainingTime.current -1
     const minutes = Math.floor(remainingTime.current/60);
     const sec = remainingTime.current%60
-    minutesElement.current.innerHTML = minutes <10? `0${minutes}`: minutes
-    secElement.current.innerHTML = sec < 10 ? `0${sec}`:sec}
+    // minutesElement.current.innerHTML = minutes <10? `0${minutes}`: minutes
+    // secElement.current.innerHTML = sec < 10 ? `0${sec}`:sec
+    const minuteToDisplay = (minutes <10? `0${minutes}`: minutes)
+    const secToDisplay= (sec < 10 ? `0${sec}`:sec)
+    minutesElement.current.innerHTML = minuteToDisplay+`:`+secToDisplay
+
+  }
       
-    },1000)
+    },100)
   
 }, [timer.play])
 
@@ -130,20 +157,20 @@ useEffect(()=>{
       </div>
       <div className='timer-container-div'>
         <h4 id='timer-label' className='timer-label-h2'>
-          {timer.turn==="session"? "Session":'Break'}
+          {timerLabel}
         </h4>
         <div id="time-left" className='time-left'>
-          <span className='minutes' ref={minutesElement}>{displayedTimer}</span>
-          :
-          <span className='seconds' ref={secElement}>{'00'}</span>
+          <span className='minutes' ref={minutesElement}>{`${displayedTimer}:00`}</span>
+          
+          {/* <span className='seconds' ref={secElement}>{'00'}</span> */}
         </div>
         </div>
           <div className='controle-panel'>
-           <button className='play-pause' onClick={handlePlayPauseClick}>
+           <button id="start_stop" className='play-pause' onClick={handlePlayPauseClick}>
               <i className="fa fa-play fa-2x"></i>
               <i className="fa fa-pause fa-2x"/>
             </button>
-           <button className='reset-btn' onClick={handleResetClick}><i className="fa fa-refresh fa-2x"></i></button>
+           <button id="reset" className='reset-btn' onClick={handleResetClick}><i className="fa fa-refresh fa-2x"></i></button>
            <audio ref={beep} id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
           </div>
         
